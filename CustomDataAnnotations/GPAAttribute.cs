4c637c3806419ec7d44ad1e4896375e0c05e3f11
Reservation <113533406+karim549549@users.noreply.github.com>
2024-06-01 18:24:43 +0300

    Add files via upload

---
+++ CustomDataAnnotations/GPAAttribute.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollageManagmentSystem.businessLogicLayer.CustomDataAnnotations
+{
+    internal class GPAAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value == null)
+            {
+                return new ValidationResult("GPA is required.");
+            }
+
+            if (double.TryParse(value.ToString(), out double gpa))
+            {
+                if (gpa <= 0 || gpa >= 4)
+                {
+                    return new ValidationResult("GPA must be between 0 and 4.");
+                }
+            }
+            else
+            {
+                return new ValidationResult("GPA must be a numeric value.");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
---
+++ CustomDataAnnotations/MajorAttribute.cs
@@ -0,0 +1,38 @@
+﻿using CollageManagmentSystem.businessLogicLayer.MagicData;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CollageManagmentSystem.businessLogicLayer.CustomDataAnnotations
+{
+    internal class MajorAttribute : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value == null)
+            {
+                return new ValidationResult("Major is Required .. ");
+            }
+            string inputValue = Regex.Replace(value.ToString().ToLower(), @"\s+", "");
+
+            foreach (string major in MagicLists.MajorList)
+            {
+                string processedMajor = Regex.Replace(major.ToLower(), @"\s+", "");
+                if (processedMajor == inputValue)
+                {
+                    return ValidationResult.Success;
+                }
+            }
+            return new ValidationResult("Invalid Major...");
+
+
+
+
+            return ValidationResult.Success;
+        }
+    }
+}
